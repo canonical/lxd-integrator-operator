3b4b1dd45f75b545e490f2bf51be178a66fce217
Enzo Aguado <enzo.aguado@canonical.com>
2021-01-05 14:12:36 +0100

    implement lxd relation
    This new 'LXD' relation is an early draft meant to fill the gap between
    any LXD providing charm and their clients. This integrator charm makes use
    of that relation
    Included is the operators framework implementation of the relation

--- src/charm.py
+++ src/charm.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 #
-# (c) 2020 Canonical Ltd. All right reservered
+# (c) 2020 Canonical Ltd. All rights reserved
 #
+
+import logging
+import json
+import subprocess
 
 from ops.charm import CharmBase
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus
 
-import logging
-import json
-import subprocess
+from interface import Lxd
+
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +23,8 @@ class LxdIntegratorCharm(CharmBase):
         self.framework.observe(self.on.install, self._on_install)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
+        self.client = Lxd(self, 'api')
+
     def _on_install(self, event):
         if not self._check_credentials():
             return
@@ -29,6 +34,9 @@ class LxdIntegratorCharm(CharmBase):
             return
 
     def _check_credentials(self):
+        if self.client.is_ready:
+            return
+
         try:
             result = subprocess.run(['credential-get', '--format=json'],
                                     check=True,
@@ -40,10 +48,11 @@ class LxdIntegratorCharm(CharmBase):
             client_key = creds['credential']['attrs']['client-key']
             server_cert = creds['credential']['attrs']['server-cert']
             if endpoint and client_cert and client_key and server_cert:
+                self.client.set_credentials(endpoint, client_cert, client_key, server_cert)
                 self.model.unit.status = ActiveStatus()
                 return True
         except json.JSONDecodeError as e:
-            logger.warn('Failed to parse JSON from credentials-get: {}'.format(e.msg))
+            logger.warning('Failed to parse JSON from credentials-get: {}'.format(e.msg))
         except FileNotFoundError:
             pass
         except subprocess.CalledProcessError as e:
@@ -55,7 +64,7 @@ class LxdIntegratorCharm(CharmBase):
         client_key = self.model.config['lxd_client_key']
         server_cert = self.model.config['lxd_server_cert']
         if endpoint and client_cert and client_key and server_cert:
-            # self.client.set_credentials(endpoint, client_cert, client_key, server_cert)
+            self.client.set_credentials(endpoint, client_cert, client_key, server_cert)
             self.model.unit.status = ActiveStatus()
             return True
 
