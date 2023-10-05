6ef3409d97f8e135aa8ee11a439246ef6130948e
Simon Fels <simon.fels@canonical.com>
2023-10-05 21:46:38 +0200

    fix(tests): correct code formatting
    This makes the linter finally happy and all code apply to the rules.

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from OpenSSL import crypto
 
+
 @pytest.fixture
 def tls_config() -> tuple[bytes, bytes]:
     # create a key pair
@@ -22,6 +23,7 @@ def tls_config() -> tuple[bytes, bytes]:
         crypto.FILETYPE_PEM, cert
     )
 
+
 @pytest.fixture
 def lxd_secret(tls_config):
     return {
@@ -35,7 +37,7 @@ def lxd_secret(tls_config):
             "authtype": "certificate",
             "attrs": {
                 "client-cert": tls_config[1].decode("utf-8"),
-                "client-key":  tls_config[0].decode("utf-8"),
+                "client-key": tls_config[0].decode("utf-8"),
                 "server-cert": "-----BEGIN CERTIFICATE-----\nMIIB/jCCAYSgAwIBAgIRAOK8FTN83SBIlyz1hVjLAukwCgYIKoZIzj0EAwMwMjEc\nMBoGA1UEChMTbGludXhjb250YWluZXJzLm9yZzESMBAGA1UEAwwJcm9vdEBqdWp1\nMB4XDTIzMDkxNDA5NTQxNVoXDTMzMDkxMTA5NTQxNVowMjEcMBoGA1UEChMTbGlu\ndXhjb250YWluZXJzLm9yZzESMBAGA1UEAwwJcm9vdEBqdWp1MHYwEAYHKoZIzj0C\nAQYFK4EEACIDYgAEciWQqXqACauT4rPZNpwsW1fX7S9wItrUO6oiC3M/byQ1/Aoa\nLeNKGm78e4lqmHNl1gaugheP+6xMH/4GvqOPSqOBMKMpZqOYQaTcb7Yo/lQkWJYS\nJkSF5ZmN7SFrN/Kco14wXDAOBgNVHQ8BAf8EBAMCBaAwEwYDVR0lBAwwCgYIKwYB\nBQUHAwEwDAYDVR0TAQH/BAIwADAnBgNVHREEIDAeggRqdWp1hwR/AAABhxAAAAAA\nAAAAAAAAAAAAAAABMAoGCCqGSM49BAMDA2gAMGUCMQCMeyH1wxJg3xK8drrC8/q5\nJChelKfH5JNydgXhrsjkqYPZg49JDTF354My+JqaCIkCMHs6CXNBE1oP4gt9QxzF\nrOwhteFjSKVcKXMHYFC7SoBjIHFFIMD4i4juNw0W0TzqeA==\n-----END CERTIFICATE-----\n",
             },
             "redacted": [],
